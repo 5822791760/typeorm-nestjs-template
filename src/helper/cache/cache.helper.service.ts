479d4f416377a7af74e253c80275b53b9d987447
Robert <robertkunakorn@gmail.com>
2025-04-25 00:26:13 +0700

    Feat: allow disable cache

--- src/helper/cache/cache.helper.service.ts
+++ src/helper/cache/cache.helper.service.ts
@@ -15,6 +15,10 @@ export class CacheHelperService {
   async get<T>(
     key: string,
   ): Promise<Res<string | T | null, 'notFound' | 'fail'>> {
+    if (!this._isEnable()) {
+      return Ok(null);
+    }
+
     try {
       const val = await this.redisClient.get(key);
       if (!val) {
@@ -33,6 +37,10 @@ export class CacheHelperService {
     data: string | Record<string, any>,
     ttlInSeconds = 3600,
   ): Promise<Res<null, 'fail'>> {
+    if (!this._isEnable()) {
+      return Ok(null);
+    }
+
     try {
       await this.redisClient.set(key, JSON.stringify(data), {
         EX: ttlInSeconds,
@@ -44,6 +52,10 @@ export class CacheHelperService {
   }
 
   async delete(key: string): Promise<Res<null, 'fail'>> {
+    if (!this._isEnable()) {
+      return Ok(null);
+    }
+
     try {
       await this.redisClient.del(key);
       return Ok(null);
@@ -53,11 +65,19 @@ export class CacheHelperService {
   }
 
   async exists(key: string): Promise<boolean> {
+    if (!this._isEnable()) {
+      return false;
+    }
+
     try {
       const exists = await this.redisClient.exists(key);
       return exists > 0;
     } catch (error) {
       return false;
     }
+  }
+
+  private _isEnable() {
+    return this.redisClient.isOpen;
   }
 }
